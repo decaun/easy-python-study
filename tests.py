10945abf308f139bba86bb7ee3c966c717d89841
decaun <denizcanuner@gmail.com>
2019-02-14 20:03:01 +0100

    tests

--- tests.py
+++ tests.py
@@ -1,18 +1,38 @@
-#from shell run python3 -m "nose" test.py
+#from shell run python3 -m "nose" tests.py
 #or 1st install nosetests3 from apt-get > sudo apt-get install python-nose python3-nose
-#then issue command nosetests3 test.py / nosetests unit_test.py
+#then issue command nosetests3 tests.py / nosetests tests.py
 
 from unittest import TestCase # 1
 from mock import patch, call # 2
 from nose.tools import assert_equal,assert_list_equal # 3
-from filter_funcs import filter_ints # 4
+from filter_funcs import filter_ints,is_positive # 4
 
 class FilterIntsTestCase(TestCase): # 5
+
     '''interface test'''
     def test_filter_ints_return_value(self):
         v = [3, -4, 0, -2, 5, 0, 8, -1]
         result = filter_ints(v)
         assert_list_equal([3, 5, 8], result)
+
+    '''interface test with triangulation'''
+    def test_filter_ints_return_value_triangular(self):
+        v1 = [3, -4, 0, -2, 5, 0, 8, -1]
+        v2 = [7, -3, 0, 0, 9, 1]
+        assert_list_equal([3, 5, 8], filter_ints(v1))
+        assert_list_equal([7, 9, 1], filter_ints(v2))
+
+    '''interface test with boundaries'''
+    def test_is_positive(self):
+        assert_equal(False, is_positive(-1)) # before boundary with granularity 1
+        assert_equal(False, is_positive(0)) # on the boundary with granularity 1
+        assert_equal(True, is_positive(1)) # after the boundary with granularity 1
+
+    def test_is_positive_v2(self):
+        assert_equal(False, is_positive(0))
+        for n in range(1, 10 ** 4):
+            assert_equal(False, is_positive(-n))
+            assert_equal(True, is_positive(n))
 
     '''unit test'''
     @patch('filter_funcs.is_positive') # 6
