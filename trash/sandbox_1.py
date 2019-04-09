8f0bbe0180d5a7a60d2885ce46dbcf3e4251e382
decaun <denizcanuner@gmail.com>
2019-04-09 11:44:44 +0200

    sandbox

---
+++ trash/sandbox_1.py
@@ -0,0 +1,35 @@
+class house():
+    door=1
+    wall=4
+    floor=1
+
+    def __init__(self):
+        self.cimney=1
+        self.window=4
+        self.toilet=1
+        self.increase_cimney()
+        
+    @classmethod
+    def increase_wall(cls):
+        cls.wall+=1
+        print("There are {} walls now".format(cls.wall))
+        
+    @property
+    def be_wall(self):
+        print("i dont do anything X {}".format(self.wall))
+        return self.wall
+    
+    @staticmethod
+    def flush(self):
+        print("flushed {} times".format(self.toilet))
+
+    def increase_cimney(self):
+        self.cimney+=1
+
+if __name__=="__main__":
+    ben=house()
+    print(ben.be_wall)
+    house.flush(ben)
+    house.increase_wall()
+    print("ben has {} walls also ".format(ben.be_wall))
+    print(ben.cimney)
