3cbfd5d944d8eee37d076f450f9696590fc7d7e1
decaun <denizcanuner@gmail.com>
2019-04-13 06:09:26 +0200

    models

--- flask/proj_1/main/models.py
+++ flask/proj_1/main/models.py
@@ -38,3 +38,14 @@ class Post(db.Model):
 
         def __repr__(self):
             return f"Post('{self.title}', '{self.date_posted}')"
+'''
+class Playlist(db.Model):
+        id = db.Column(db.Integer, primary_key=True)
+        title = db.Column(db.String(100), nullable=False)
+        date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+        content = db.Column(db.Text, nullable=False)
+        user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+        def __repr__(self):
+            return f"Post('{self.title}', '{self.date_posted}')"
+'''
