d1e5974ee42fb79829c6105aec80c69d65bef3f3
decaun <denizcanuner@gmail.com>
2019-04-15 12:09:54 +0200

    Topic page set

--- flask/proj_1/main/routes.py
+++ flask/proj_1/main/routes.py
@@ -1,19 +1,25 @@
 from main.models import User, Post, Playlist
 from main.forms import PostForm
 from main import app,db
-from flask import render_template,jsonify,request,url_for
+from flask import render_template,jsonify,request,url_for,redirect
 
 
 @app.route("/", methods=['GET', 'POST'])
-def home():
-    posts = Post.query.all()
+@app.route("/<int:playlist_id>", methods=['GET', 'POST'])
+def Topic(playlist_id=None):
     playlists = Playlist.query.all()
+    if playlist_id==None:
+        playlist_selected=Playlist.query.get(1)
+    else:
+        playlist_selected=Playlist.query.get_or_404(playlist_id)
+    posts = playlist_selected.posts
     form = PostForm()
     if form.validate_on_submit():
-        post = Post(title=form.title.data, content=form.content.data, user_id=1, playlist_id=1 )
+        post = Post(title=form.title.data, content=form.content.data, user_id=1, playlist_id=playlist_selected.id )
         db.session.add(post)
         db.session.commit()
-    return render_template('home.html',posts=posts,form=form, playlists=playlists, legend='New Post')
+        return redirect(url_for('Topic', playlist_id=playlist_selected.id))
+    return render_template('home.html',posts=posts,form=form, playlists=playlists,playlist_selected=playlist_selected, legend='New Post')
 
 @app.route("/topic")
 def topic():
--- flask/proj_1/run.py
+++ flask/proj_1/run.py
@@ -2,7 +2,7 @@
 #$env:FLASK_DEBUG=1
 #$env:FLASK_RUN_PORT=80 - bi sike yaramadi
 #flask run --host=127.0.0.1 --port=80
-#python -m flask run --host=127.0.0.1 --port=5000
+#python -m flask run --host=127.0.0.1 --port=80
 from main import app
 
 if __name__=='__main__':
