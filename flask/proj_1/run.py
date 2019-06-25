912caceb814923c48353e4576d1cca58f1ab100b
decaun <denizcanuner@gmail.com>
2019-06-25 02:56:49 +0200

    Im back !!!!!

--- flask/proj_1/run.py
+++ flask/proj_1/run.py
@@ -7,5 +7,5 @@
 from main import app
 
 if __name__=='__main__':
-    app.run(debug=True)
-    #app.run(host = '127.0.0.1',port=80)
+    #app.run(debug=True)
+    app.run(host = '127.0.0.1',port=80)
