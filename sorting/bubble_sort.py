14e7161f2c3bb4dc7318b28857a1769be01db063
decaun <denizcanuner@gmail.com>
2019-07-27 21:47:51 -0500

    set stuff

--- sorting/bubble_sort.py
+++ sorting/bubble_sort.py
@@ -6,3 +6,4 @@ def bubble_sort(array):
         for j in range(len(array) - 1):
             if array[j] > array[j + 1]:
                 array[j], array[j + 1] = array[j + 1], array[j]  # swap
+    return array
