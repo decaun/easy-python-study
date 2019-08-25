30e218bfd7b1eb3ff7b2547eadfc490162100c06
decaun <denizcanuner@gmail.com>
2019-08-25 21:10:29 +0200

    rX

---
+++ reactiveX/simple_create.py
@@ -0,0 +1,24 @@
+import rx
+
+def push_five_strings(observer,scheduler):
+    observer.on_next("Alpha")
+    observer.on_next("Beta")
+    observer.on_next("Gamma")
+    observer.on_next("Delta")
+    observer.on_next("Epsilon")
+    observer.on_completed()
+
+class PrintObserver(rx.core.Observer):
+
+    def on_next(self, value):
+        print("Received {0}".format(value))
+
+    def on_completed(self):
+        print("Done!")
+
+    def on_error(self, error):
+        print("Error Occurred: {0}".format(error))
+
+source = rx.create(push_five_strings)
+
+source.subscribe(PrintObserver())
