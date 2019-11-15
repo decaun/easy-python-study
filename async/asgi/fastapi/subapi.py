78fb0c2f7a4a683cde19b7826fe0515c9d5c090a
Deniz Can Üner <denizcanuner@gmail.com>
2019-11-15 04:46:41 +0000

    subdoc

---
+++ async/asgi/fastapi/subapi.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+
+app = FastAPI(openapi_prefix="/subapi")
+
+
+@app.get("/app")
+def read_main():
+    return {"message": "Hello World from main app"}
+    
+app.mount("/subapi", app)
