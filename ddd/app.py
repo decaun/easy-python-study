92d68f877ed54f6071ed241bc6e42120e05accc8
Deniz Can Üner <denizcanuner@gmail.com>
2019-12-11 20:59:32 +0000

    app starte

---
+++ ddd/app.py
@@ -0,0 +1,25 @@
+from eventsourcing.utils.random import encoded_random_bytes
+
+# Keep this safe (random bytes encoded with Base64).
+cipher_key = encoded_random_bytes(num_bytes=32)
+
+from eventsourcing.application.sqlalchemy import SQLAlchemyApplication
+from eventsourcing.domain.model.aggregate import AggregateRoot
+
+application = SQLAlchemyApplication(
+    uri='sqlite:///:memory:',
+    cipher_key=cipher_key,
+    persist_event_type=AggregateRoot.Event,
+)
+
+from eventsourcing.infrastructure.eventstore import EventStore
+
+assert isinstance(application.event_store, EventStore)
+
+from eventsourcing.application.policies import PersistencePolicy
+
+assert isinstance(application.persistence_policy, PersistencePolicy)
+
+from eventsourcing.infrastructure.eventsourcedrepository import EventSourcedRepository
+
+assert isinstance(application.repository, EventSourcedRepository)
